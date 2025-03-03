eb79615d2f87cbe2e3b22038d3ed26f133bcc267
jungbin97 <ljb5054@gmail.com>
2025-03-04 02:52:40 +0900

    refactor: 사용하지 않는 import정리

--- src/test/java/webserver/processor/StaticResourceProcessorTest.java
+++ src/test/java/webserver/processor/StaticResourceProcessorTest.java
@@ -1,6 +1,5 @@
 package webserver.processor;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import webserver.http11.request.HttpRequest;
