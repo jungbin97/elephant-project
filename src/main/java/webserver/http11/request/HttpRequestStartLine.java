a2ebf288e84e779870313b7792104e1c0587abb0
jungbin97 <ljb5054@gmail.com>
2025-03-06 23:03:44 +0900

    refactor: requestUri 쿼리파라미터 붙어있는 형태로원본 유지

--- src/main/java/webserver/http11/request/HttpRequestStartLine.java
+++ src/main/java/webserver/http11/request/HttpRequestStartLine.java
@@ -1,29 +1,16 @@
 package webserver.http11.request;
 
-import util.HttpRequestUtils;
-
-import java.util.Map;
 
 public class HttpRequestStartLine {
 
     private final String method;
     private final String requestUri;
     private final String httpVersion;
-    private final Map<String, String> queryParams;
 
     public HttpRequestStartLine(String method, String requestUri, String httpVersion) {
         this.method = method;
         this.httpVersion = httpVersion;
-        // Parse query parameters
-        int queryIndex = requestUri.indexOf("?");
-        if (queryIndex != -1) {
-            this.requestUri = requestUri.substring(0, queryIndex);
-            String queryString = requestUri.substring(queryIndex + 1);
-            this.queryParams = HttpRequestUtils.parseQueryString(queryString);
-        } else {
-            this.requestUri = requestUri;
-            this.queryParams = Map.of();
-        }
+        this.requestUri = requestUri;
     }
 
     public String getMethod() {
@@ -38,12 +25,8 @@ public class HttpRequestStartLine {
         return httpVersion;
     }
 
-    public Map<String, String> getQueryParams() {
-        return queryParams;
-    }
-
     @Override
     public String toString() {
-        return "RequestStartLine [method=" + method + ", uri=" + requestUri + ", httpVersion=" + httpVersion + "]";
+        return "HttpRequestStartLine [method=" + method + ", requestUri=" + requestUri + ", httpVersion=" + httpVersion + "]";
     }
 }
